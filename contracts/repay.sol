5ec97f13fe0e53c0994c1eee2327247e131089e5
A7 <danieladeniyi17@outlook.com>
2025-04-20 02:15:08 +0100

    yo wth

--- contracts/repay.sol
+++ contracts/repay.sol
@@ -4,45 +4,41 @@ pragma solidity ^0.8.0;
 contract RepaymentDeed {
     address public lender;
     address public borrower;
-    mapping(address => uint32) public balances; // Mapping to track balances of each address
-    uint32 private totalLoan;
-    uint32 private interestRate; // in percentage
-    uint32 private startDate;
-    uint32 private duration;
-    uint32 private interval;
+    mapping(address => uint) public balances; // Mapping to track balances of each address
+    uint private totalLoan;
+    uint private interestRate; // in percentage
+    uint private startDate;
+    uint private duration;
+    uint private instalments;
 
-    constructor(address _lender, uint32 _totalLoan, uint32 _interestRate, uint32 _startDate, uint32 _duration, uint32 _interval) {
+    constructor(address _lender, uint _totalLoan, uint _interestRate, uint _duration, uint _instalments) {
       borrower = msg.sender;
       lender = _lender;
       totalLoan = _totalLoan;
       interestRate = _interestRate;
-      startDate = _startDate;
+      startDate = block.timestamp;
       duration = _duration;
-      interval = _interval;
+      instalments = _instalments;
     }
 
-    event PaymentMade(address indexed borrower, uint32 amount, uint32 remainingBalance);
-    event LoanRepaid(address indexed borrower, uint32 totalLoan, uint32 interestRate, uint32 remainingBalance);
+    event PaymentMade(address indexed borrower, uint amount, uint remainingBalance);
+    event LoanRepaid(address indexed borrower, uint totalLoan, uint interestRate, uint remainingBalance);
 
-    error InvalidPayment(uint32 attempted, uint32 available);
+    error InvalidPayment(uint attempted, uint available);
 
-    function pay(uint32 _amount) public payable {
+    function pay(uint _amount) public payable {
       require(msg.sender == borrower, "Only borrower can make payments");
       require(_amount <= balances[borrower], InvalidPayment(_amount, balances[borrower]));
-      _amount = totalLoan*(1+interestRate)/interval;
+      _amount = totalLoan*(1+interestRate)/instalments;
       balances[borrower] -= _amount;
       balances[lender] += _amount;
       emit PaymentMade(borrower, _amount, getRemainingBalance());
     }
-    function getLoanDetails() public view returns (address, address, uint32, uint32, uint32, uint32) {
+    function getLoanDetails() public view returns (address, address, uint, uint, uint, uint) {
         return (lender, borrower, totalLoan, interestRate, startDate, duration);
     }
 
-    function getPaymentDetails() public view returns (uint32) {
-        return interval;
-    }
-
-    function getRemainingBalance() public view returns (uint32) {
+    function getRemainingBalance() public view returns (uint) {
         return totalLoan - (totalLoan * interestRate / 100);
     }
 }
